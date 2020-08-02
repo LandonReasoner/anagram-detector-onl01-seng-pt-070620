1e31096f68889d5563c98c56d1be9453e364eab9
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-02 14:21:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+    match = %w(Anagram.new)
+  end
+  
+  def match(array)
+    collection = []
+    array.collect do |x|
+      if x.sort == @word.sort
+        return collection << x
+      else 
+        collection
+      end
+    end
+    collection
+  end
+end
